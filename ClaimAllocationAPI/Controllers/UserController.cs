b67dacb48f0b350bf1cc66c56136a48b0203eb5a
Mukesh Vyas <mukeshvyas@rathi.com>
2023-07-25 18:18:52 +0530

    Add MVC Project

---
+++ ClaimAllocationAPI/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using ClaimEntity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClaimAllocationAPI.Controllers
+{
+    public class UserController : Controller
+    {
+        DataContext _DataContext;
+        public IActionResult Index(IConfiguration configuration)
+        {
+
+            return View();
+        }
+
+        [HttpGet(Name = "GetUser")]
+        public IEnumerable<User> Get()
+        {
+            DataContext DB = new DataContext();
+            return DB.Users.ToArray();
+            //var lst = 
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = Random.Shared.Next(-20, 55),
+            //    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            //})
+            //.ToArray();
+        }
+    }
+}
---
+++ ClaimEntity/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaimEntity
+{
+    public class User
+    {
+        public Int64 Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime DateOfCreation { get; set; }
+                
+    }
+}
